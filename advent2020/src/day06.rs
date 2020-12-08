f962f7650e670ed25a7883d6bc42e3cfe95ece24
Pedro de Oliveira <falsovsky@gmail.com>
2020-12-08 01:29:26 +0000

    Add Day 6 in rust

---
+++ advent2020/src/day06.rs
@@ -0,0 +1,46 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::collections::HashMap;
+
+fn main() {
+    let mut entries: Vec<Vec<String>> = Vec::new();
+    let f = File::open("../day06/input").unwrap();
+    let file = BufReader::new(&f);
+    let mut tmp: Vec<String> = Vec::new();
+    for line in file.lines() {
+        let clean = line.unwrap().to_string();
+        if clean == "" {
+            entries.push(tmp);
+            tmp = Vec::new()
+        } else {
+            tmp.push(clean);
+        }
+    }
+    entries.push(tmp);
+
+    let mut part1 = 0;
+    let mut part2 = 0;
+    for group in entries {
+        let mut questions: HashMap::<char, u16> = HashMap::new();
+        for person in &group {
+            for (_, c) in person.chars().enumerate() {
+                let mut times: u16 = 0;
+                if questions.contains_key(&c) {
+                    times = *questions.get(&c).unwrap();
+                }
+                times +=1;
+                questions.remove(&c);
+                questions.insert(c, times);
+            }
+        }
+        part1 += questions.len();
+        for (_, value) in questions {
+            if value == group.len() as u16 {
+                part2 += 1;
+            }
+        }
+    }
+    println!("part1: {}", part1);
+    println!("part2: {}", part2);
+}
