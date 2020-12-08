238030a9b49a5be5fd837bf0dad309574cf5c149
Pedro de Oliveira <falsovsky@gmail.com>
2020-12-08 22:22:10 +0000

    Optimize day 7, no regex, still sucks.

--- advent2020/src/day07.rs
+++ advent2020/src/day07.rs
@@ -1,26 +1,24 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
+#![feature(test)]
 
-use regex::Regex;
+extern crate test;
+
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-lazy_static! {
-    static ref RE_NAME: Regex = Regex::new(r"([\w ]+) bags contain").unwrap();
-    static ref RE_CONTENTS: Regex = Regex::new(r"(\d+) ([\w ]+) bags?").unwrap();
-}
-
 fn baggins(entries: &Vec<String>, name: String, types: &mut Vec<String>) {
     for bag in entries {
-        let bagname = &bag[..bag.find(" bags contain").unwrap()];
-        for content in RE_CONTENTS.captures_iter(&bag) {
-            if content[2] == name {
-                if types.contains(&bagname.to_string()) == false {
-                    types.push(bagname.to_string());
+        let split = &bag.split(" ").collect::<Vec<&str>>();
+        let bagname = format!("{} {}", split[0], split[1]);
+        if split[4] != "no" {
+            for foo in (4..split.len()).step_by(4) {
+                let zbr = format!("{} {}", split[foo + 1], split[foo + 2]);
+                if zbr == name {
+                    if types.contains(&bagname.to_string()) == false {
+                        types.push(bagname.to_string());
+                    }
+                    baggins(&entries, bagname.to_string(), types);
                 }
-                baggins(&entries, bagname.to_string(), types);
             }
         }
     }
@@ -29,15 +27,14 @@ fn baggins(entries: &Vec<String>, name: String, types: &mut Vec<String>) {
 fn baggins2(entries: &Vec<String>, name: String, number: u32) -> u32 {
     let mut total = 0;
     for bag in entries {
-        for bagname in RE_NAME.captures_iter(&bag) {
-            if bagname[1] == name {
-                for content in RE_CONTENTS.captures_iter(&bag) {
-                    total += number
-                        * baggins2(
-                            &entries,
-                            content[2].to_string(),
-                            content[1].parse::<u32>().unwrap(),
-                        );
+        let split = &bag.split(" ").collect::<Vec<&str>>();
+        let bagname = format!("{} {}", split[0], split[1]);
+        if bagname == name {
+            if split[4] != "no" {
+                for foo in (4..split.len()).step_by(4) {
+                    let subname = format!("{} {}", split[foo + 1], split[foo + 2]);
+                    let subvalue = split[foo].parse::<u32>().unwrap();
+                    total += number * baggins2(&entries, subname, subvalue);
                 }
             }
         }
@@ -60,4 +57,9 @@ fn main() {
         "part2: {}",
         baggins2(&entries, "shiny gold".to_string(), 1) - 1
     );
+}
+
+#[bench]
+fn bench_day07(b: &mut test::Bencher) {
+    b.iter(|| main());
 }
